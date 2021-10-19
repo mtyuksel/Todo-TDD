4fe8be0e60e9755f39786ac396aaa78198c61a44
mtyuksel <yukselmtevfik@gmail.com>
2021-10-20 00:57:23 +0300

    add dump components

---
+++ frontend/src/components/TaskAdd.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="task-add">
+    <input class="inpAdd" placeholder="Enter a Task..." type="text" />
+    <button class="btnAdd">Add Task</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TaskAdd",
+};
+</script>
+
+<style scoped>
+</style>
---
+++ frontend/src/components/TaskList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="task-list">
+<ul class="taskList">
+    <li>Task - 1</li>
+    <li>Task - 2</li>
+    <li>Task - 3</li>
+    <li>Task - 4</li>
+</ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TaskAdd",
+};
+</script>
+
+<style scoped>
+</style>
