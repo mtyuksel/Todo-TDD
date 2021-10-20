c02af1705b17a76fb4108d160d2fd7f2ce73ceeb
mtyuksel <yukselmtevfik@gmail.com>
2021-10-20 15:37:38 +0300

    add todo service mock for pact test

---
+++ frontend/tests/mocks/TodoServiceMock.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+export async function getAll() {
+    return axios.get('http://localhost:9001/tasks/getall');
+}
+
+export async function add(taskName) {
+    body = {name: taskName}
+    return axios.post('http://localhost:9001/tasks/add', body);
+}
