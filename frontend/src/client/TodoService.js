db08244023bc89d7536dcae0d6d55ea184bebde4
mtyuksel <yukselmtevfik@gmail.com>
2021-10-20 15:23:47 +0300

    add service client

--- frontend/src/client/TodoService.js
+++ frontend/src/client/TodoService.js
@@ -1,9 +1,14 @@
-import axios from 'axios'
+// import axios from 'axios'
 
 export async function getAll() {
-    return axios.get('http://localhost:9000/tasks/getall');
+    return [{id: "1", name: "test1"},{id: "2", name: "test2"}]
+    // return axios.get('http://localhost:9000/tasks/getall');
 }
 
 export async function add(task) {
-    return axios.post('http://localhost:9000/tasks/add', task);
+    return {
+        id: "1123",
+        name: task
+    }
+    // return axios.post('http://localhost:9000/tasks/add', task);
 }
