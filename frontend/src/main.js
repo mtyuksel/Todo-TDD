c4502db4b9e25c15ddef71fde6540c4c9e2200e5
mtyuksel <yukselmtevfik@gmail.com>
2021-10-20 15:23:58 +0300

    add event bus

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,14 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+export const eventBus = new Vue({
+  methods: {
+    addTask(task) {
+      this.$emit("taskAdded", task)
+    }
+  }
+});
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
