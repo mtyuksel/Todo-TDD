3409ee82f6135fbaf0c31af712077b76d8464715
mtyuksel <yukselmtevfik@gmail.com>
2021-10-20 15:33:49 +0300

    refactor todo service add body to add request

--- frontend/src/client/TodoService.js
+++ frontend/src/client/TodoService.js
@@ -1,14 +1,10 @@
-// import axios from 'axios'
+import axios from 'axios'
 
 export async function getAll() {
-    return [{id: "1", name: "test1"},{id: "2", name: "test2"}]
-    // return axios.get('http://localhost:9000/tasks/getall');
+    return axios.get('http://localhost:9000/tasks/getall');
 }
 
-export async function add(task) {
-    return {
-        id: "1123",
-        name: task
-    }
-    // return axios.post('http://localhost:9000/tasks/add', task);
+export async function add(taskName) {
+    body = {name: taskName}
+    return axios.post('http://localhost:9000/tasks/add', body);
 }
