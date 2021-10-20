87a62b4f5c7592cd32693b74c8eac66a21f64daa
mtyuksel <yukselmtevfik@gmail.com>
2021-10-20 15:09:09 +0300

    refactor add component

--- frontend/src/components/TaskAdd.vue
+++ frontend/src/components/TaskAdd.vue
@@ -1,15 +1,46 @@
 <template>
-  <div class="task-add">
-    <input class="inpAdd" placeholder="Enter a Task..." type="text" />
-    <button class="btnAdd">Add Task</button>
+  <div id="task-add">
+    <button @click="addTask" class="btnAdd">Add Task</button>
+    <input
+      v-model="taskName"
+      class="inpAdd"
+      type="text"
+      placeholder="Enter a Task..."
+    />
   </div>
 </template>
 
 <script>
+import { eventBus } from "../main";
+import { add } from "../client/TodoService";
+
 export default {
-  name: "TaskAdd",
+  data: function () {
+    return {
+      taskName: "",
+    };
+  },
+  methods: {
+    addTask: function () {
+      if (this.isTaskContainsStar(this.taskName)) {
+        alert("Tasks cannot contains stars!");
+        return;
+      }     
+      add(this.taskName)
+        .then((res) => {
+          eventBus.addTask(res);
+        })
+        .catch((err) => {
+          console.log("An error occured!", err);
+        });
+    },
+    isTaskContainsStar: function (taskName) {
+      return taskName.includes("*");
+    },
+  },
+  created() {},
 };
 </script>
 
-<style scoped>
+<style>
 </style>
